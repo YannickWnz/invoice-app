e5f8c8ab6da319c5257a41fa712a3969819acf13
yannickwnz <yannickwnz@gmail.com>
2023-04-26 18:08:34 +0000

    app.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ import Invoice from './Pages/Invoice/Invoice'
 import Navbar from './Components/Navbar/Navbar';
 import CreateInvoice from './Components/CreateInvoice/CreateInvoice';
 import NewInvoice from './Components/CreateInvoice/NewInvoice';
+import { ThemeProvider } from './context/Context';
 
 function App() {
   return (
-    <div className="App">
-      {/* <h1>whats poppin homeboy</h1> */}
-      <Navbar />
-      {/* <CreateInvoice /> */}
-      {/* <NewInvoice /> */}
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} ></Route>
-          <Route path='/invoice/:id' element={<Invoice />} ></Route>
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider> 
+      <div className="App">
+        {/* <h1>whats poppin homeboy</h1> */}
+        <Navbar />
+        {/* <CreateInvoice /> */}
+        {/* <NewInvoice /> */}
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} ></Route>
+            <Route path='/invoice/:id' element={<Invoice />} ></Route>
+          </Routes>
+        </Router>
+      </div>
+    </ThemeProvider> 
+
   );
 }
 
