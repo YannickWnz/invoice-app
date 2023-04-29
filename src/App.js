60bb3ab8670926690aa67b9d4e2209972ceaa571
yannickwnz <yannickwnz@gmail.com>
2023-04-29 16:37:47 +0000

    private route path added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import {useState} from 'react'
 import Home from './Pages/Home/Home'
 import Invoice from './Pages/Invoice/Invoice'
@@ -10,6 +10,7 @@ import NewInvoice from './Components/CreateInvoice/NewInvoice';
 import { ThemeProvider } from './context/Context';
 import NotFound from './Pages/404/NotFound';
 import Register from './Pages/Register/Register';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
@@ -21,8 +22,16 @@ function App() {
         {/* <NewInvoice /> */}
         <Router>
           <Routes>
-            <Route path='/' element={<Home />} ></Route>
-            <Route path='/invoice/:id' element={<Invoice />} ></Route>
+            <Route path='/' element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            } ></Route>
+            <Route path='/invoice/:id' element={
+              <PrivateRoute>
+                <Invoice />
+              </PrivateRoute>
+            } ></Route>
             <Route path='/login' element={ <Login /> } ></Route>
             <Route path='/register' element={ <Register /> } ></Route>
             <Route path='*' element={ <NotFound /> } ></Route>
